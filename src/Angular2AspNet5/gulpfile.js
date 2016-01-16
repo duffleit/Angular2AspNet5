de824c06cf41f55fb09beeb9e6ae02c3a306ffe6
David <davidleitner@live.at>
2016-01-16 18:40:47 +0100

    updated gulp task to copy frontend npm dependencies

--- src/Angular2AspNet5/gulpfile.js
+++ src/Angular2AspNet5/gulpfile.js
@@ -8,51 +8,49 @@ var runSequence = require('run-sequence');
 
 var paths = {
     webroot: "./" + project.webroot + "/",
-    nodemodules: "./node_modules/",
-    www_libs: "libs/",
-    www_index: "index.html"
+    www_index: "index.html",
+    www_external: "externals/"
 };
 
-var npmFrontendDependencies = [
-    "systemjs/dist/system.js",
-    "systemjs/dist/system-polyfills.js",
-    "angular2/bundles/angular2-polyfills.js",
-    "rxjs/bundles/Rx.js",
-    "angular2/bundles/angular2.js"
-];
+var frontedNpmDependencies = {
+    js: [
+        "./node_modules/systemjs/dist/system.js",
+        "./node_modules/systemjs/dist/system-polyfills.js",
+        "./node_modules/angular2/bundles/angular2-polyfills.js",
+        "./node_modules/rxjs/bundles/Rx.js",
+        "./node_modules/angular2/bundles/angular2.js"
+    ],
+    css: [
+        "node_modules/bootstrap/dist/css/bootstrap.min.css"
+    ]
+}
 
-//internal tasks:
+gulp.task('cleanFrontedNpmDependencies', function () {
+    return gulp.src(paths.webroot + paths.www_external, { read: false }).pipe(clean());
 
-gulp.task('cleanWwwLibsFolder', function () {
-    return gulp.src(paths.webroot + paths.www_libs, { read: false }).pipe(clean());
 });
 
-gulp.task('moveNodeFrontendDependenciesToLib', function () {
-    var absolutePathToFrontedDependencies = [];
-    for (var index = 0; index < npmFrontendDependencies.length; index++) {
-        absolutePathToFrontedDependencies[index] = paths.nodemodules + npmFrontendDependencies[index];
+gulp.task('copyFrontedNpmDependencies', function () {
+    gulp.src(frontedNpmDependencies.js).pipe(gulp.dest(paths.webroot + paths.www_external + "js/"));
+    gulp.src(frontedNpmDependencies.css).pipe(gulp.dest(paths.webroot + paths.www_external + "css/"));
+});
+
+gulp.task('injectFrontedNpmDependencies', function () {
+    var dependencyPathes = [];
+    for (var jsIndex = 0; jsIndex < frontedNpmDependencies.js.length; jsIndex++) {
+        var splittedJsName = frontedNpmDependencies.js[jsIndex].split('/');
+        dependencyPathes[dependencyPathes.length] = paths.webroot + paths.www_external + "js/" + splittedJsName[splittedJsName.length - 1];
+    }
+    for (var cssIndex = 0; cssIndex < frontedNpmDependencies.css.length; cssIndex++) {
+        var splittedCssName = frontedNpmDependencies.css[cssIndex].split('/');
+        dependencyPathes[dependencyPathes.length] = paths.webroot + paths.www_external + "css/" + splittedCssName[splittedCssName.length - 1];
     }
 
-    return gulp.src(absolutePathToFrontedDependencies).pipe(gulp.dest(paths.webroot + paths.www_libs));
+    gulp.src(paths.webroot + paths.www_external)
+        .pipe(inject(gulp.src(dependencyPathes, { read: false }), { relative: true }))
+        .pipe(gulp.dest(paths.webroot));
 });
 
-gulp.task('injectNodeFrontendDependenciesIntoIndex', function () {
-    var absolutePathToFrontedDependencies = [];
-    for (var index = 0; index < npmFrontendDependencies.length; index++) {
-        var splittedPath = npmFrontendDependencies[index].split('/');
-        absolutePathToFrontedDependencies[index] = paths.webroot + paths.www_libs + splittedPath[splittedPath.length-1];
-    }
-    var targetIndexHtml = paths.webroot + paths.www_index;
-
-    gulp.src(targetIndexHtml)
-      .pipe(inject(gulp.src(absolutePathToFrontedDependencies, { read: false }), { relative: true }))
-      .pipe(gulp.dest(paths.webroot));
-
-});
-
-
-//public tasks:
-
-gulp.task("updateFrontedDependencies", function () {
-    runSequence("cleanWwwLibsFolder", "moveNodeFrontendDependenciesToLib", "injectNodeFrontendDependenciesIntoIndex")
+gulp.task("updateFrontedNpmDependencies", function () {
+    runSequence("cleanFrontedNpmDependencies", "copyFrontedNpmDependencies", "injectFrontedNpmDependencies");
 });
